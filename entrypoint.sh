e968700c2c7e87f6190ff0baf2da9392b934678b
ShadowGlass0 <gkecskem@gmail.com>
2021-10-08 00:31:28 +1100

    Fix lint errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 sed -i "s|write-data=.*|write-data=$GITHUB_WORKSPACE/factorio|g" /opt/factorio/config/config.ini
 
-exec /opt/factorio/bin/x64/factorio --mod-directory $GITHUB_WORKSPACE/factorio/mods --create $GITHUB_WORKSPACE/factorio/saves/test01
+exec /opt/factorio/bin/x64/factorio --mod-directory "$GITHUB_WORKSPACE/factorio/mods" --create "$GITHUB_WORKSPACE/factorio/saves/test01"
